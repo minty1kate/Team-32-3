1bb4b8942f3e5338ab12b2bace65eada7603dffa
Your Name <you@example.com>
2026-03-30 20:03:04 +0500

    Игрок и ходьба

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        player = GameObject.Find("character_0");
+    }
+
+    void Update()
+    {
+        var pos = player.transform.position;
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+    }
+
+    private GameObject player;
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float acceleration = 30;
+    private Rigidbody2D rigidBodyComponent;
+    private GameObject exit;
+    
+    void Start()
+    {
+        rigidBodyComponent = GetComponent<Rigidbody2D>();
+        exit = GameObject.Find("Exit");
+    }
+    
+    void Update()
+    {
+        var w = Input.GetKey(KeyCode.W) ? 1 : 0;
+        var a  = Input.GetKey(KeyCode.A) ? 1 : 0;
+        var s = Input.GetKey(KeyCode.S) ? -1 : 0;
+        var d = Input.GetKey(KeyCode.D) ? -1 : 0;
+        var movement = new Vector2(-(a + d), w + s);
+        
+        rigidBodyComponent.linearVelocity = movement * acceleration;
+        if (movement.magnitude > Mathf.Epsilon)
+        {
+            var angle = new Vector3(0 ,0, movement.magnitude);
+            transform.rotation = Quaternion.Euler(angle);
+        }
+    }
+}
