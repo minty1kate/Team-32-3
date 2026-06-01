c1fc9a8116a09618ccd78db6ab93ece1f2ff6c50
minty1kate <you@example.com>
2026-06-01 23:57:37 +0500

    Main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Этот модуль нужен для управления сценами
+
+public class MainMenu : MonoBehaviour
+{
+    // Этот метод сработает при нажатии на кнопку "Начать игру"
+    public void OnStartButtonClick()
+    {
+        // Переключает сцену на следующую по списку в настройках игры
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Этот метод сработает при нажатии на кнопку "Выход"
+    public void OnQuitButtonClick()
+    {
+        // Выводит сообщение в консоль Unity, чтобы мы видели, что кнопка работает
+        Debug.Log("Игра закрывается...");
+
+        // Полностью закрывает запущенную игру
+        Application.Quit();
+    }
+}
